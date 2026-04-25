83262df5be81b39e9941e89776a3ff84351e3055
nasir-masud <cactas.28@gmail.com>
2026-04-25 20:08:00 +0600

    Complete leftSide

--- src/app/components/homePage/LeftSide.jsx
+++ src/app/components/homePage/LeftSide.jsx
@@ -4,11 +4,12 @@ const LeftSide = ({ categories, activeId }) => {
   return (
     <ul className='flex flex-col text-slate-400 gap-2 py-1.5'>
       {categories.news_category.map((category) => (
-        <li
-          className={`btn btn-ghost py-3 text-xl justify-start ${activeId === category.category_id ? "font-bold" : "font-normal"}`}
-          key={category.category_id}
-        >
-          <Link href={`/category/${category.category_id}`}>
+        <li key={category.category_id}>
+          <Link
+            href={`/category/${category.category_id}`}
+            className={`btn btn-ghost py-3 text-xl justify-start w-full 
+      ${activeId === category.category_id ? "font-bold" : "font-normal"}`}
+          >
             {category.category_name}
           </Link>
         </li>
