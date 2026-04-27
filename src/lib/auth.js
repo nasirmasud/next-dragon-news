22ce7568dbd65752a8f468024f085e944371195d
nasir-masud <cactas.28@gmail.com>
2026-04-27 22:00:50 +0600

    Better Auth & mongoDB configured

---
+++ src/lib/auth.js
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_CONNECT_STRING);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
+});
