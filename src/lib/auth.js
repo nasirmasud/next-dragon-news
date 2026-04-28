9948d11e7ea4d5202323941e98b22343a8b2f7ab
nasirmasud <nasir.masud@ymail.com>
2026-04-29 00:22:35 +0600

    Register & login info save to mongodb

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,12 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_CONNECT_STRING);
-const db = client.db();
+const db = client.db("next-dragon-news");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
